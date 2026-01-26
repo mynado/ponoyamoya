aec9c249398a1dcf9c3008c734b20d26cdee9603
MyNa Do <myna.do@bimobject.com>
2026-01-26 13:36:05 +0100

    fix: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,38 @@
 import "./globals.css";
 import Header from "./components/Header";
 import FooterWrapper from "./components/FooterWrapper";
+import { Metadata } from "next";
 
-export const metadata = {
-  title: "Ponoyamoya",
-  description: "Artist & Healer",
+export const metadata: Metadata = {
+  title: "Pono ya Moya: Anti-Disciplinary Platform for Ancestral Healing",
+  description:
+    "Pono ya Moya is Nkgono Mantwa's practice (sangoma & moporofeta), channeled with and through Mmabatho Thobejane. Spiritual counsel centered on ancestral healing.",
+  keywords: [
+    "Pono ya Moya",
+    "Mmabatho Thobejane",
+    "ancestral healing",
+    "sangoma",
+    "moporofeta",
+    "spiritual counsel",
+    "healing practices ",
+    "traditional healer",
+  ],
+  openGraph: {
+    title: "Pono ya Moya: Anti-Disciplinary Platform for Ancestral Healing",
+    description:
+      "Pono ya Moya is Nkgono Mantwa's practice (sangoma & moporofeta), channeled with and through Mmabatho Thobejane. Spiritual counsel centered on ancestral healing.",
+    url: "https://ponoyamoya.com",
+    siteName: "Pono ya Moya",
+    images: [
+      {
+        url: "https://ponoyamoya.com/og-altar-session.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Pono ya Moya",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -13,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="sv" data-scroll-behavior="smooth">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className="bg-white text-gray-900 min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">{children}</main>
