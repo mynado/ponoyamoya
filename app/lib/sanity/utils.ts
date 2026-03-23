b3a2d958598d2e9db4ec98a235935e5542dd3d69
MyNa Do <myna.do@bimobject.com>
2026-03-23 20:49:40 +0100

    fix: env

--- app/lib/sanity/utils.ts
+++ app/lib/sanity/utils.ts
@@ -1,5 +1,5 @@
 const getImageUrl = (imageRef: string) => {
-  const sanityCdnUrl = `https://cdn.sanity.io/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/production/`;
+  const sanityCdnUrl = `${process.env.NEXT_PUBLIC_SANITY_CDN_URL}/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/production/`;
   const [type, assetId, dimensions, format] = imageRef.split("-");
   return `${sanityCdnUrl}${assetId}-${dimensions}.${format}`;
 };
