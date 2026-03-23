be7956288a7240763cd4b026e22af73a5146b793
MyNa Do <myna.do@bimobject.com>
2026-03-23 20:49:30 +0100

    fix: error handling

--- app/lib/sanity/queries.ts
+++ app/lib/sanity/queries.ts
@@ -3,21 +3,34 @@ import { EventData, JournalPostData, OfferingsData, PageData } from "./types";
 
 // Pages
 export async function getPage(heading: string): Promise<PageData | null> {
-  return sanityClient.fetch(`*[_type == "pages" && heading == $heading][0]`, {
-    heading,
-  });
+  return sanityClient
+    .fetch(`*[_type == "pages" && heading == $heading][0]`, {
+      heading,
+    })
+    .catch((error) => {
+      console.error(`Error fetching page with heading "${heading}":`, error);
+      return null;
+    });
 }
 
 // Events
 export async function getUpcomingEvents(): Promise<EventData[]> {
-  return sanityClient.fetch(
-    `*[_type == "events" && endTime >= now()] | order(startTime asc)`,
-  );
+  return sanityClient
+    .fetch(`*[_type == "events" && endTime >= now()] | order(startTime asc)`)
+    .catch((error) => {
+      console.error("Error fetching upcoming events:", error);
+      return [];
+    });
 }
 
 // Offerings
 export async function getOfferings(): Promise<OfferingsData[]> {
-  return sanityClient.fetch(`*[_type == "offerings"] | order(_createdAt asc)`);
+  return sanityClient
+    .fetch(`*[_type == "offerings"] | order(_createdAt asc)`)
+    .catch((error) => {
+      console.error("Error fetching offerings:", error);
+      return [];
+    });
 }
 
 // Journal Posts
@@ -27,8 +40,9 @@ export async function getJournalPostBySlug(
 ): Promise<JournalPostData | null> {
   const client = isEnabled ? sanityClientWithToken : sanityClient;
 
-  return client.fetch(
-    `*[_type == "journalPost" && slug.current == $slug][0]{
+  return client
+    .fetch(
+      `*[_type == "journalPost" && slug.current == $slug][0]{
       title,
       slug,
       body,
@@ -40,14 +54,18 @@ export async function getJournalPostBySlug(
         slug
       }
     }`,
-    { slug },
-    isEnabled
-      ? {
-          perspective: "drafts",
-          useCdn: false,
-        }
-      : undefined,
-  );
+      { slug },
+      isEnabled
+        ? {
+            perspective: "drafts",
+            useCdn: false,
+          }
+        : undefined,
+    )
+    .catch((error) => {
+      console.error(`Error fetching journal post with slug "${slug}":`, error);
+      return null;
+    });
 }
 
 export async function getAllJournalPosts(
